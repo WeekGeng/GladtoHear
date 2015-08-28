8072c708ef6fb27858874a38ade0e3719f1af312
WeekGeng <13162833922@163.com>
2015-08-28 12:12:01 +0800

    3

--- app/src/main/java/com/yieryi/gladtohear/activities/ProductsListActivity.java
+++ app/src/main/java/com/yieryi/gladtohear/activities/ProductsListActivity.java
@@ -1,13 +1,12 @@
 package com.yieryi.gladtohear.activities;
-
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.TextView;
-
 import com.jauker.widget.BadgeView;
 import com.yieryi.gladtohear.R;
 import com.yieryi.gladtohear.adapter.ShopingCarAdapter;
@@ -26,13 +25,16 @@ public class ProductsListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_products_list);
         initView();
         initToolbar();
+        setView();
+    }
 
+    private void setView() {
         recyclerView.setLayoutManager(manager);
         recyclerView.setAdapter(adapter);
         badgeView.setTargetView(shopping_car_tv);
         badgeView.setBadgeCount(3);
-
     }
+
     private void initToolbar() {
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -47,5 +49,15 @@ public class ProductsListActivity extends AppCompatActivity {
         manager=new LinearLayoutManager(this);
         shopping_car_tv=(TextView)findViewById(R.id.shopping_car_tv);
         badgeView=new BadgeView(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+            break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
