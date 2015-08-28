ea2feb96333f0beea9812ecbd9909614faddf338
WeekGeng <13162833922@163.com>
2015-08-28 12:06:26 +0800

    3

--- app/src/main/java/com/yieryi/gladtohear/activities/ProductsListActivity.java
+++ app/src/main/java/com/yieryi/gladtohear/activities/ProductsListActivity.java
@@ -1,9 +1,11 @@
 package com.yieryi.gladtohear.activities;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 import com.jauker.widget.BadgeView;
@@ -16,18 +18,28 @@ public class ProductsListActivity extends AppCompatActivity {
     private LinearLayoutManager manager;
     private TextView shopping_car_tv;
     private BadgeView badgeView;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products_list);
         initView();
+        initToolbar();
         recyclerView.setLayoutManager(manager);
         recyclerView.setAdapter(adapter);
         badgeView.setTargetView(shopping_car_tv);
         badgeView.setBadgeCount(3);
 
     }
-
+    private void initToolbar() {
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("品牌选择");
+        toolbar.setTitleTextColor(Color.WHITE);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
     private void initView() {
         recyclerView=(RecyclerView)findViewById(R.id.recycle_market);
         adapter=new ShopingCarAdapter(this);
