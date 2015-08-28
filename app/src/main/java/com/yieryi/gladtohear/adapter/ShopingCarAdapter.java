69f95e9e5ecb3070dbc6f4915dbbc3463acc7c72
WeekGeng <13162833922@163.com>
2015-08-28 12:04:36 +0800

    3

--- app/src/main/java/com/yieryi/gladtohear/adapter/ShopingCarAdapter.java
+++ app/src/main/java/com/yieryi/gladtohear/adapter/ShopingCarAdapter.java
@@ -1,17 +1,24 @@
 package com.yieryi.gladtohear.adapter;
 
 import android.content.Context;
+import android.graphics.Paint;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.jauker.widget.BadgeView;
 import com.yieryi.gladtohear.R;
 /**
  * Created by Administrator on 2015/8/26.
  */
 public class ShopingCarAdapter extends RecyclerView.Adapter<ShopingCarAdapter.MyHolder>{
-    LayoutInflater inflater;
+    private LayoutInflater inflater;
+    private Context context;
     public ShopingCarAdapter(Context context){
+        this.context=context;
         inflater=LayoutInflater.from(context);
     }
     @Override
@@ -24,7 +31,7 @@ public class ShopingCarAdapter extends RecyclerView.Adapter<ShopingCarAdapter.My
             v=LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.marketsel_name_item, parent, false);
         }
-        MyHolder vh = new MyHolder(v);
+        MyHolder vh = new MyHolder(v,viewType);
         return vh;
     }
 
@@ -35,15 +42,29 @@ public class ShopingCarAdapter extends RecyclerView.Adapter<ShopingCarAdapter.My
 
     @Override
     public void onBindViewHolder(MyHolder holder, int position) {
-
+        if (getItemViewType(position)==1) {
+            holder.market_content_item_old_price_tv.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG); //中间横线
+            BadgeView badgeView = new BadgeView(context);
+            badgeView.setTargetView(holder.tv_sheng);
+            badgeView.setBadgeGravity(Gravity.TOP | Gravity.LEFT);
+//            badgeView.setTypeface(Typeface.create(Typeface.SANS_SERIF,
+//                    Typeface.ITALIC));
+            badgeView.setText("省");
+        }
     }
     @Override
     public int getItemCount() {
         return 10;
     }
     class MyHolder extends RecyclerView.ViewHolder{
-        public MyHolder(View itemView) {
+        private TextView tv_sheng;
+        private TextView market_content_item_old_price_tv;
+        public MyHolder(View itemView,int viewType) {
             super(itemView);
+            if (viewType==1) {
+                tv_sheng = (TextView) itemView.findViewById(R.id.shopping_sheng_tv);
+                market_content_item_old_price_tv = (TextView) itemView.findViewById(R.id.market_content_item_old_price_tv);
+            }
         }
     }
 }
